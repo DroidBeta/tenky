77f6843f8074c3af5bf1b93e50960fd10b6a4c5f
深度学习 <33132228+Kucashu@users.noreply.github.com>
2019-12-21 19:22:22 +0800

    Extension: add some method
    Extension: add some method
    Security: build
    Web: build

---
+++ tenky/Extension.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DroidBeta.Tenky.Extension
+{
+    public static class CharExtension
+    {
+        /// <summary>
+        /// Convert char[] to string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string ToNewString(this char[] input) => new string(input);
+    }
+
+    public static class StringExtension
+    {
+
+        /// <summary>
+        /// Replace with dual value.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="newValue"></param>
+        /// <param name="oldValue"></param>
+        /// <returns></returns>
+        public static string Replace(this string input, string[] oldValue, string newValue)
+        {
+            foreach(string oldValueLoop in oldValue)
+            {
+                input = input.Replace(oldValueLoop, newValue);
+            }
+            return input;
+        }
+
+        /// <summary>
+        /// Determine whether the string is int.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IsInt(this string input)
+        {
+            try
+            {
+                Int32.Parse(input);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Capitalize the first letter of the string.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string Capitalize(this string str) => str.Substring(0, 1).ToUpper() + str[1..].ToLower();
+
+    }
+
+    public static class IntExtension
+    {
+        /// <summary>
+        /// Convert int? to int.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static int ToInt(this int? input) => input.GetValueOrDefault();
+    }
+}
---
+++ tenky/Web.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DroidBeta.Tenky.Web
+{
+
+}
