befeda55b87edc5c308cc624cf64140c59cc4978
深度学习 <33132228+Kucashu@users.noreply.github.com>
2019-12-21 19:21:15 +0800

    sln: upload basic things.
    Program: just for test

---
+++ tenky/Program.cs
@@ -0,0 +1,21 @@
+﻿/********************************************************
+ * Tenky
+ * by DroidBeta Dev Team
+ * A stupid library for dotnet.
+ ********************************************************
+ * This file is just for test.
+ *******************************************************/
+
+using System;
+using DroidBeta.Tenky.Extension;
+
+namespace tenky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
