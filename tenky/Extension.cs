85cf1136b43d0f56fec884790ce782297b3c05d9
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2020-01-12 14:00:49 +0800

    Extension: add int&string method
    string: ToInt();
    int: IsImei();
    int: ToImei15();
    int: GetLength();
    int: SubInt();
    int: Append();

--- tenky/Extension.cs
+++ tenky/Extension.cs
@@ -51,6 +51,8 @@ namespace DroidBeta.Tenky.Extension
             }
         }
 
+        public static int ToInt(this string input) => int.Parse(input);
+
         /// <summary>
         /// Capitalize the first letter of the string.
         /// </summary>
@@ -68,5 +70,59 @@ namespace DroidBeta.Tenky.Extension
         /// <param name="input"></param>
         /// <returns></returns>
         public static int ToInt(this int? input) => input.GetValueOrDefault();
+
+        public static bool IsImei(this int imei)
+        {
+            if (imei.GetLength() != 15)
+                return false;
+            else
+                return imei == imei.SubInt(0, 14).ToImei15();
+        }
+
+        public static int ToImei15(this int imei14)
+        {
+            
+            if(imei14.GetLength() != 14)
+            {
+                throw new ArgumentOutOfRangeException(imei14 + " is not 14 bit!");
+            }
+
+            int sum = 0;
+            int[] ME = new int[14];
+            //split string into a char array
+            for (int i = 0; i < 14; i++)
+            {
+                ME[i] = Convert.ToInt32(imei14.SubInt(i, 1));
+            }
+            int[] ME_Even = new int[7];
+            for (int i = 0; i < 7; i++)
+                ME_Even[i] = ME[i * 2 + 1] * 2;
+            int[] ME_Odd = new int[7];
+            for (int i = 0; i < 7; i++)
+                ME_Odd[i] = ME[i * 2];
+            for (int i = 0; i < 7; i++)
+                sum += ME_Odd[i] + ME_Even[i] / 10 + ME_Even[i] % 10;
+            if (sum % 10 == 0)
+                sum = 0;
+            else
+                sum = 10 - sum % 10;
+
+            return imei14.Append(sum);
+        }
+
+        public static int GetLength(this int interger) => interger.ToString().Length;
+
+        public static int SubInt(this int num, int startIndex) => num.SubInt(startIndex, num.GetLength() - 1);
+        public static int SubInt(this int num, int startIndex, int length) => int.Parse(num.ToString().Substring(startIndex, length));
+
+        public static int Append(this int firstNum, params int[] restNums)
+        {
+            string tmp = firstNum.ToString();
+            foreach(int restNum in restNums)
+            {
+                tmp += restNum.ToString();
+            }
+            return tmp.ToInt();
+        }
     }
 }
