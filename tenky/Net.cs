91a1095806ef90d35c69e914066680f406cfb60c
isKevinTrue <203377325@qq.com>
2020-01-17 10:29:45 +0800

    Add GetStr();
    CHG: Get(); -> GetResponse();
    ADD: GetStr();

--- tenky/Net.cs
+++ tenky/Net.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Net;
 using System.IO;
@@ -9,7 +8,32 @@ namespace DroidBeta.Tenky.Net
     class Http
     {
         private const string _defaultUA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36";
-        public static string Get(string url, int? timeoutSecond, string userAgent, string referer, CookieCollection cookies)
+        
+        public static string GetStr(string url) => GetStr(url, null, null, null, null);
+
+        public static string GetStr(string url, int timeoutSecond) => GetStr(url, timeoutSecond, null, null, null);
+
+        public static string GetStr(string url, int? timeoutSecond, string userAgent, string referer, CookieCollection cookies)
+        {
+            WebResponse response = GetResponse(url, timeoutSecond, userAgent, referer, cookies);
+
+            Stream stream = response.GetResponseStream();
+            string responseStr = "";
+            if (stream != null)
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                responseStr = reader.ReadToEnd();
+                reader.Close();
+            }
+            response.Close();
+            return responseStr;
+        }
+
+        public static WebResponse GetResponse(string url) => GetResponse(url, null, null, null, null);
+
+        public static WebResponse GetResponse(string url, int timeoutSecond) => GetResponse(url, timeoutSecond, null, null, null);
+
+        public static WebResponse GetResponse(string url, int? timeoutSecond, string userAgent, string referer, CookieCollection cookies)
         {
             if (string.IsNullOrEmpty(url))  
                 throw new ArgumentNullException(url + " is not a valid url.");
@@ -31,22 +55,9 @@ namespace DroidBeta.Tenky.Net
                 request.CookieContainer.Add(cookies);
             }
 
-            WebResponse response = request.GetResponse();
-            Stream stream = response.GetResponseStream();
-            string responseStr = "";
-            if (stream != null)
-            {
-                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
-                responseStr = reader.ReadToEnd();
-                reader.Close();
-            }
-            response.Close();
-            return responseStr;
+            return request.GetResponse();
         }
 
-        public static string Get(string url) => Get(url, null, null, null, null);
-
-        public static string Get(string url, int timeoutSecond) => Get(url, timeoutSecond, null, null);
     }
 
 }
